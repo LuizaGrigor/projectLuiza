90faaf38c97a31e561b1414ea13816e708cd7492
Luiza <luizagrigormarinela@gmail.com>
2024-08-02 00:20:44 +0300

    Am incarcat o parte din exercitii.

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,9 +1,22 @@
 package org.example;
 
-public class Calculator {
-    public int addNumber(int numberX, int numberY) {
-        int result = numberX + numberY;
-        return result;
+public class Calculator
+{
+    public int addNumber(int numberX, int numberY)
+    {
+        return numberX + numberY;
+    }
 
+    public int substractionNumber(int numberX, int numberY)
+    {
+        return numberX - numberY;
+    }
+    public int multiplicationionNumber(int numberX, int numberY)
+    {
+        return numberX * numberY;
+    }
+    public int divisionNumber(int numberX, int numberY)
+    {
+        return numberX / numberY;
     }
 }
--- src/main/java/org/example/LogicalOp.java
+++ src/main/java/org/example/LogicalOp.java
@@ -1,12 +1,39 @@
 package org.example;
 
 public class LogicalOp {
-    public int getHigherNumber(int firstNumber, int secondNumber) {
-        if (firstNumber > secondNumber)
+    public int checkBiggerNumber(int firstNumber, int secondNumber) {
+        if (firstNumber > secondNumber) {
             return firstNumber;
-        else
+        } else {
             return secondNumber;
+        }
+    }
 
+    public String checkText(String anyString) {
+        if (anyString.equals("Evozon")) {
+            return "Learning text comparison";
+        } else {
+            return "Got to try some more";
+        }
+    }
+
+    public String compareTextAndNumber(String anyString, int anyInt) {
+
+        String compareText = "Evozon";
+
+        if (anyString.equals(compareText) && anyInt <= 3) {
+            return anyString + " " + anyInt;
+        } else if (!anyString.equals(compareText) && anyInt > 4) {
+            return anyInt + " " + anyString;
+        } else {
+            return "Missing condition.";
+        }
+    }
+
+    public void countToHundred(int number) {
+        for (int i = number; i <= 100; i++) {
+            System.out.println(i);
+        }
     }
 
     public void countDownToHundred(int number) {
@@ -16,17 +43,51 @@ public class LogicalOp {
     }
 
 
-    public void countDownBetweenParameters(){
-        int number1 = 100;
-        int number2 = 50;
+    public void countBetweenParameters(int number1, int number2) {
 
-        int max = getHigherNumber(number1, number2);
-        int min = 2;
+        int max = checkBiggerNumber(number1, number2);
+        int countNumbers = 0;
+        int min = 0;
 
-        while (max>=min){
-            System.out.println(max);
-            max--;
+        if (max == number1) {
+            min = number2;
+        } else {
+            min = number1;
+        }
+
+        for(int i = min; i <= max; i++) {
+            countNumbers += i;
+        }
+
+        System.out.println(countNumbers);
+    }
+    public void countBetweenTwoParameters(int number1, int number2) {
+
+       int countNumbers = 0;
+
+       if (number1 > number2) {
+           for(int i = number2; i <= number1; i++) {
+               countNumbers += i;
+           }
+           System.out.println(countNumbers);
+           return;
+       }
+        System.out.println("Missing condition.");
+    }
+
+    public void countDownToHundredUpWhile(int number) {
+
+        while (number >= 100) {
+            System.out.println(number);
+            number--;
         }
     }
 
+    public void countDownToHundredWhile(int number) {
+
+        while (number >= -100) {
+            System.out.println(number);
+            number--;
+        }
+    }
 }
